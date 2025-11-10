adbd793a02b0208d3a96c5bcfc131acaa04f2887
jay4244 <mokariyajay86@gmail.com>
2025-11-10 12:32:59 +0530

    modified:   Introdction/conditional/Lists/List_Project.py modified:   Introdction/conditional/Lists/List_Project.py

--- Introdction/conditional/Lists/List_Project.py
+++ Introdction/conditional/Lists/List_Project.py
@@ -3,7 +3,7 @@ Team = ["CSK","MI","RCB","KKR","SRH","DC","RR","PBKS","LSG","GT"]
 print("Teams are :",Team)
 your_Team = int(input("Enter your team :"))
 if your_Team in Team:
-        print("You have selected :",Team[your_Team])
+        print("You have selected :",Team)
 
 
 opponent = random.choice(Team)
