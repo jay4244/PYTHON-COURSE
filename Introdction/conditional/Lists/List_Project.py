8a48acef549ba934753b8f95d6a72cd6299e570a
jay4244 <mokariyajay86@gmail.com>
2025-11-10 12:25:12 +0530

    new file:   Introdction/conditional/Lists/List_Project.py modified:   Introdction/conditional/While.ipynb

---
+++ Introdction/conditional/Lists/List_Project.py
@@ -0,0 +1,10 @@
+import random
+Team = ["CSK","MI","RCB","KKR","SRH","DC","RR","PBKS","LSG","GT"]
+print("Teams are :",Team)
+your_Team = int(input("Enter your team :"))
+if your_Team in Team:
+        print("You have selected :",Team[your_Team])
+
+
+opponent = random.choice(Team)
+print("Your opponent is :",opponent)
