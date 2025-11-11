104fdb0f34158dae4aa792d901d82939f5258642
jay4244 <mokariyajay86@gmail.com>
2025-11-11 15:01:13 +0530

    new file:   Introdction/conditional/Lists/List.py 	new file:   Introdction/conditional/Lists/Practical.ipynb 	new file:   Introdction/conditional/Lists/tuple.ipynb 	modified:   Introdction/introduction.txt

---
+++ Introdction/conditional/Lists/List.py
@@ -0,0 +1,9 @@
+"""
+nested list compresion
+"""
+l1 = [10 , 20 ,[ 1, 2, 3, ["python","java"]], 30 , 40, 50, ["android","flutter"],["php"]]
+
+print(l1[1])
+print(l1[2][1])
+print(l1[2][3][1])
+print(l1[6][1])
