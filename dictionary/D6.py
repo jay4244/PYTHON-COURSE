febf42e2b720d22f0262c96cefe03934b0bac2dd
jay4244 <mokariyajay86@gmail.com>
2025-11-21 12:34:51 +0530

    modified:   dictionary/D6.py

--- dictionary/D6.py
+++ dictionary/D6.py
@@ -24,18 +24,30 @@ while main_status:
             product[product_name]=sub_dis
             
             print(product)
+            break
     elif choice==2:
-        sub_dis={}
+        status = True
         while status:
             sub_dis={}
-            Customer_name =input("Enter product name :")
-            product_qty = int(input("Enter your product qty :"))
-            product_price =int(input("Enter product price :"))
+            print("Available products are :")
+            for key,value in product.items():
+                print(f"Product name : {key} | price : {value['price']} | qty : {value['qty']}")
+            Customer_name =input("Enter Customer name :")
+            Customer_product =input("Enter product name :")
+            Customer_qty = int(input("Enter your product qty :"))
             
-            sub_dis["qty"]=product_qty
-            sub_dis["price"]=product_price
-            
-        
-            print(costomer)   
+            sub_dis ={
+                "Customer_name" :Customer_name,
+                "product" : Customer_product,
+                "qty" : Customer_qty,
+                "Bill" : product_price*Customer_qty,
+            }
+            break
+        print("-----------------------------------------")
+        print("--------------Customer Bill--------------")
+        print("-----------------------------------------")
+        for key,value in sub_dis.items():
+            print(f"{key}: {value}") 
+        print("-----------------------------------------")         
     else:
         main_status=False        
