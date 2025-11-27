fd861a31c100a6210c2518ca518fcdc77dde4204
jay4244 <mokariyajay86@gmail.com>
2025-11-27 15:53:28 +0530

    modified:   Function/mini_project.py

--- Function/mini_project.py
+++ Function/mini_project.py
@@ -1,59 +1,65 @@
-db ={} # main database
+db = {}  # main database
 
-def registration(name,mail,password,subject,city):
+def registration(name, mail, password, subject, city):
     global db
-    sub_dis ={}
-    sub_dis["name"] = name 
-    sub_dis["subject"] = subject
-    sub_dis["mail"] = mail
-    sub_dis["password"] = password
-    sub_dis["city"] = city
-    
-    if mail in db.keys():
-        return "user already exists !."
+    sub_dis = {
+        "name": name,
+        "subject": subject,
+        "mail": mail,
+        "password": password,
+        "city": city
+    }
+
+    if mail in db:
+        return "User already exists!"
     else:
-        return "succesfuly registration done ."
-    
-def login(mail,password):
+        db[mail] = sub_dis     
+        return "Successfully registered!"
+
+
+def login(mail, password):
     global db
-    if mail in db.keys():
+    if mail in db:
         if password == db[mail]["password"]:
-            return f"welcome {db[mail]["name"]}"
+            return f"Welcome {db[mail]['name']}!"
         else:
-            return "password does not match"
+            return "Password does not match"
     else:
-        return "invalid mail address"
-    
-menu = """
-        1)registration
-        2)login
+        return "Invalid email address"
 
-"""        
+
+menu = """
+1) Registration
+2) Login
+"""
+
 status = True
 while status:
     print(menu)
-    choice = int(input("Enter your chioce :"))
+    choice = int(input("Enter your choice: "))
+
     if choice == 1:
-        name = input("Enter your name :")
-        mail = input("Enter your mail :")
-        password = int(input("Enter your password :"))
-        subject = input("Enter your subject :")
-        city = input("Enter your city :")
-        
-    res = registration(name,mail,password,subject,city)
-    
-    print("Response :",res)
-    
-else:
-    mail = input("Enter your mail :")
-    password = input ("Enter your password :")
-    
-    res = login(mail,password)
-    
-    print(res) 
-    
+        name = input("Enter your name: ")
+        mail = input("Enter your email: ")
+        password = input("Enter your password: ")
+        subject = input("Enter your subject: ")
+        city = input("Enter your city: ")
+
+        res = registration(name, mail, password, subject, city)
+        print("Response:", res)
+
+    elif choice == 2:
+        mail = input("Enter your email: ")
+        password = input("Enter your password: ")
+
+        res = login(mail, password)
+        print("Response:", res)
+
+    else:
+        print("Invalid choice!")
+
     print("--------------------------")
-    
-    choice_exit = input("do you wnat so perform more operations ? press 'y' or 'n'").lower()
-    if choice_exit == 'n' or choice_exit == 'no':
-        status =False   
+    choice_exit = input("Do you want to perform more operations? (y/n): ").lower()
+
+    if choice_exit == "n":
+        status = False
